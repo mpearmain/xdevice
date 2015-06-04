b5834bfa222626009dcfb2dfe8a37cc4d91368a5
Mike Pearmain <michael.pearmain@gmail.com>
2015-06-04 16:51:34 +0800

    Added a training and validation split method

--- cv/cv.R
+++ cv/cv.R
@@ -1,4 +1,16 @@
-the definition of how to cut a train / validation set of data.
-This means we can ensemble smarter
-build good cv to not waste LB submissions
---- its good practice :)
+# Initial method of cutting the training set to have a validation set.
+library(data.table)
+
+full.train <- fread("./data/dev_train_basic.csv")
+# Lets cut 10% randomly
+set.seed(1401081)
+full.train[, rnd := runif(dim(full.train)[1])]
+
+train <- full.train[rnd > 0.1, ]
+validation <- full.train[rnd <= 0.1, ]
+
+train[, rnd := NULL]
+validation[, rnd := NULL]
+
+write.csv(train, './data/train_basic90.csv', row.names = F, quote = F)
+write.csv(validation, './data/valid_basic10.csv', row.names = F, quote = F)  
