42a209bc9c3947479bc2718822ee1f6308cc86a4
Konrad Banachewicz <konrad@Konrads-MacBook-Pro.local>
2015-06-06 01:57:14 +0200

    scrubbing command for id_all_ip.csv

--- command_line/basic.sh
+++ command_line/basic.sh
@@ -1,1 +1,3 @@
 sed 's/,/;/' ./data/id_all_property.csv | sed 's/,/;/; s/(//g; s/)//g; s/{//g; s/}//g; s/,[0-9][0-9][0-9],/,/g ; s/,[0-9][0-9],/,/g;  s/,[0-9],/,/g; s/,[0-9]//;  s/www.www.auctionzip.ca/property_999999/ ; s/property/prp/g;  '  > ./data/id_all_property_no_count.csv
+
+cat ./data/id_all_ip.csv | sed 's/,/;/ ; s/,/;/; s/{//g; s/}//g; ' > ./data/id_all_ip_v2.csv
