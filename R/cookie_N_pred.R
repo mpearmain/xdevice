17837fc1dbdd39dab2b77ce4b14fc75ea0ee732f
mpearmain <michael.pearmain@gmail.com>
2015-06-06 11:30:43 +0800

    Ammendments to produce # cookies for test set

--- R/cookie_N_pred.R
+++ R/cookie_N_pred.R
@@ -7,7 +7,7 @@
 library(data.table)
 
 # Fetch the latest nightly build using Jo-fai Chow's package
-devtools::install_github("woobe/deepr")
+# devtools::install_github("woobe/deepr")
 deepr::install_h2o()
 library(h2o)
 
@@ -16,6 +16,8 @@ source('./cv/cv.R') #to create data sets for reproducibility.
 
 train <- fread('./data/train_basic90.csv', stringsAsFactors = T)
 valid <- fread('./data/valid_basic10.csv', stringsAsFactors = T)
+full.train <- fread('./data/dev_train_basic_manips.csv', stringsAsFactors = T)
+test <- fread('./data/dev_test_basic_manips.csv', stringsAsFactors = T)
 
 train[, device_type_f := as.factor(device_type_f)]
 train[, device_os_f:= as.factor(device_os_f)]
@@ -28,6 +30,18 @@ valid[, device_os_f:= as.factor(device_os_f)]
 valid[, country_f := as.factor(country_f)]
 valid[, anonymous_c1_f := as.factor(anonymous_c1_f)]
 valid[, anonymous_c2_f := as.factor(anonymous_c2_f)]
+
+full.train[, device_type_f := as.factor(device_type_f)]
+full.train[, device_os_f:= as.factor(device_os_f)]
+full.train[, country_f := as.factor(country_f)]
+full.train[, anonymous_c1_f := as.factor(anonymous_c1_f)]
+full.train[, anonymous_c2_f := as.factor(anonymous_c2_f)]
+
+test[, device_type_f := as.factor(device_type_f)]
+test[, device_os_f:= as.factor(device_os_f)]
+test[, country_f := as.factor(country_f)]
+test[, anonymous_c1_f := as.factor(anonymous_c1_f)]
+test[, anonymous_c2_f := as.factor(anonymous_c2_f)]
 
 
 # Read cookie data.
@@ -47,24 +61,27 @@ valid[, cookie_cnt := as.factor(cookie_cnt)]
 
 # Build a quick Random Forest model to predict number of cookies.
 #
-# randomForest here is painful as it takes a Looooooong time, hence h20 its also easy to switch to 
+# randomForest here is painful as it takes a Looooooong time, hence h20 its also easy to switch to
 # gbm if required.
 
-localH2O = h2o.init(max_mem_size = "24g", nthreads = 12)
+localH2O = h2o.init(max_mem_size = "20g", nthreads = 8)
 
 x0_train <- as.h2o(train[, c(3:12), with = FALSE])
 x0_valid <- as.h2o(valid[, c(3:12), with = FALSE])
+x0_full_train <- as.h2o(full.train[, c(3:12), with = FALSE])
+x0_test <- as.h2o(test[, c(3:12), with = FALSE])
 
-rf.h20 <- h2o.gbm(x = names(x0_train)[1:ncol(x0_train) -1], 
+
+gbm.h20 <- h2o.gbm(x = names(x0_train)[1:ncol(x0_train) -1],
                   learn_rate = 0.3,
                   y = names(x0_train)[ncol(x0_train)],
-                  training_frame = x0_train,
-                  validation_frame = x0_valid,
+                  training_frame = x0_full_train,
                   ntrees = 1000)
-y_pred <- as.data.frame(h2o.predict(rf.h20, x0_valid))
-valid[, preds :=y_pred$predict]
+y_pred <- as.data.frame(h2o.predict(gbm.h20, x0_test))
+test[, preds := y_pred$predict]
+
+write.csv(test, './data/dev_test_basic_N_cookies.csv', row.names = F)
 
 
 
 
-                           
--- cv/cv.R
+++ cv/cv.R
@@ -2,11 +2,12 @@
 library(data.table)
 
 full.train <- fread("./data/dev_train_basic.csv")
+test <- fread("./data/dev_test_basic.csv")
 
 # First lets get rid of drawbridge handle == -1
 full.train <- full.train['drawbridge_handle' != -1, ]
 
-# Let convert some of these strings to ints.
+# Let convert some of these strings to factors.
 full.train[, device_type_f := as.factor(sub('devtype_', '', device_type))]
 full.train[, device_os_f := as.factor(sub('devos_', '', device_os))]
 full.train[, country_f := as.factor(sub('country_', '', country))]
@@ -14,6 +15,15 @@ full.train[, anonymous_c1_f := as.factor(sub('anonymous_c1_', '', anonymous_c1))
 full.train[, anonymous_c2_f := as.factor(sub('anonymous_c2_', '', anonymous_c2))]
 
 full.train[, c('device_type', 'device_os', 'country', 'anonymous_c1', 'anonymous_c2') := NULL]
+## repeat for test set.
+test[, device_type_f := as.factor(sub('devtype_', '', device_type))]
+test[, device_os_f := as.factor(sub('devos_', '', device_os))]
+test[, country_f := as.factor(sub('country_', '', country))]
+test[, anonymous_c1_f := as.factor(sub('anonymous_c1_', '', anonymous_c1))]
+test[, anonymous_c2_f := as.factor(sub('anonymous_c2_', '', anonymous_c2))]
+
+test[, c('device_type', 'device_os', 'country', 'anonymous_c1', 'anonymous_c2') := NULL]
+
 
 # Lets cut 10% randomly
 set.seed(1401081)
@@ -27,5 +37,7 @@ validation[, rnd := NULL]
 
 write.csv(train, './data/train_basic90.csv', row.names = F)
 write.csv(validation, './data/valid_basic10.csv', row.names = F)
+write.csv(full.train, './data/dev_train_basic_manips.csv', row.names = F)
+write.csv(test, './data/dev_test_basic_manips.csv', row.names = F)
+rm('full.train', 'test', 'train', 'validation')
 
-rm(full.train)
