04bc04bfbde61831ccda344866c900896d961b2b
Konrad Banachewicz <konrad@Konrads-MacBook-Pro.local>
2015-06-06 11:15:28 +0200

    select two most popular cookies based on ip presence count (= nof records for this cookie in id_all_ip), segmented by {country, c0, c1, c2}

--- R/btb.R
+++ R/btb.R
@@ -17,6 +17,17 @@
     x
   }
   
+  # pick top 2 most popular cookies
+  myFun2 <- function(x)
+  {
+    xtab <- data.frame(x)[,c("cookie_id", "nof_ips")]
+    xtab <- xtab[ order(xtab$nof_ips, decreasing = T), ]
+    top_cookies <- paste(xtab$cookie_id[1],  xtab$cookie_id[2]) 
+    x$cookie_id <- rep(top_cookies,nrow(x))
+    x
+  }
+  
+  
   # F 0.5 score
   f05score <- function(true, pred)
   {
@@ -34,30 +45,30 @@
   dev_train_basic <- fread("data/dev_train_basic.csv",stringsAsFactors =FALSE)
   dev_test_basic <- fread("data/dev_test_basic.csv",stringsAsFactors =FALSE)
   cookie_all_basic <- fread("data/cookie_all_basic.csv",stringsAsFactors =FALSE)
-  id_property <- fread('./data/id_property_top20_noCounts.csv', stringsAsFactors = F)
+  id_ip <- fread('./data/id_ip_counts.csv', stringsAsFactors = F)
   
 }
 
 ################################################################################################
 # data processing
 {
-  device_property <- id_property[device_or_cookie_indicator == 0,]
-  device_property$device_or_cookie_indicator <- NULL
-  colnames(device_property)[1] <- "device_id"
-  cookie_property <- id_property[device_or_cookie_indicator == 1,]
-  cookie_property$device_or_cookie_indicator <- NULL
-  colnames(cookie_property)[1] <- "cookie_id"
+  device_ip <- id_ip[device_or_cookie_indicator == 0,]
+  device_ip$device_or_cookie_indicator <- NULL
+  colnames(device_ip)[1] <- "device_id"
+  cookie_ip <- id_ip[device_or_cookie_indicator == 1,]
+  cookie_ip$device_or_cookie_indicator <- NULL
+  colnames(cookie_ip)[1] <- "cookie_id"
   
   # attach cookie property info to cookie_all_basic
-  cookie_all_basic <- merge(cookie_all_basic, cookie_property, by = "cookie_id", all.x = T)
+  cookie_all_basic <- merge(cookie_all_basic, cookie_ip, by = "cookie_id", all.x = T)
   cookie_all_basic[is.na(cookie_all_basic)] <- -1
   
   # attach device property to dev_train_basic
-  dev_train_basic <- merge(dev_train_basic, device_property, by = "device_id", all.x = T)
+  dev_train_basic <- merge(dev_train_basic, device_ip, by = "device_id", all.x = T)
   dev_train_basic[is.na(dev_train_basic)] <- -1
   
   # attach device property to dev_test_basic
-  dev_test_basic <- merge(dev_test_basic, device_property, by = "device_id", all.x = T)
+  dev_test_basic <- merge(dev_test_basic, device_ip, by = "device_id", all.x = T)
   dev_test_basic[is.na(dev_test_basic)] <- -1
   
   
@@ -66,10 +77,11 @@
 ################################################################################################
 # build submission
 {
-  keycols <- c("country", "anonymous_c0", "anonymous_c1", "anonymous_c2", "prp_451009")
+  keycols <- c("country", "anonymous_c0", "anonymous_c1", "anonymous_c2")
   xsummary <- ddply(cookie_all_basic, 
-                    .(country, anonymous_c0, anonymous_c1, anonymous_c2, prp_451009), 
-                    .fun=myFun)
+                    .(country, anonymous_c0, anonymous_c1, anonymous_c2), 
+                    .fun=myFun2)
+
   xsummary <- xsummary[,c(keycols, "cookie_id")]
   xsummary <- unique(xsummary)
   xsummary <- data.table(xsummary)
@@ -83,7 +95,7 @@
   xtest <- merge(dev_test_basic,xsummary,all.x=TRUE)
   
   xsub <- data.frame(xtest)[,c("device_id","cookie_id")]
-   write.csv(xsub,file="submissions/sub_country_c0_c1_c2_p451009.csv",
+   write.csv(xsub,file="submissions/sub_country_c0_c1_c2_2mostFreq.csv",
             row.names=FALSE, quote = F)
 
   
